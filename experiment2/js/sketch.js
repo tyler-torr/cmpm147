f1d045fb77fad9e8199cf196e2b6659934c2994a
tyler-torr <tylertorrella@gmail.com>
2025-04-15 00:48:00 -0700

    AHH
    AHHH

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,6 +1,6 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Starry Night Sky, which simulates a night sky with stars that rotate around the bottom of the screen
+// Author: Tyler Torrella
+// Date: 4/15/2025
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -14,14 +14,6 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
-let moonX;
-let moonY;
-let circles = [];
-let treeLayers = [];
-
-let spinSpeed = 0;
-let targetSpeed = 0;
-let rotationAngle = 0;
 
 class MyClass {
     constructor(param1, param2) {
@@ -41,6 +33,22 @@ function resizeScreen() {
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
   // redrawCanvas(); // Redraw everything based on new size
 }
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  console.log("Resizing...");
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  // redrawCanvas(); // Redraw everything based on new size
+}
+
+let moonX;
+let moonY;
+let circles = [];
+let treeLayers = [];
+
+let spinSpeed = 0;
+let targetSpeed = 0;
+let rotationAngle = 0;
 
 // setup() function is called once when the program starts
 function setup() {
@@ -58,22 +66,27 @@ function setup() {
   });
   resizeScreen();
 
+  background(21, 0, 63);
+  stroke(255);
+  noFill();
+
   for (let i = 0; i < 150; i++) {
     let x = random(-width, width);
     let y = random(-height, height);
     circles.push([x, y]);
   }
-  background(21, 0, 63);
-  stroke(255);
-  noFill();
-  
   moonX = random(-width/2, width/2);
   moonY = random(-height/5, -5*height/6);
 
   generateTrees();
 }
 
+// draw() function is called repeatedly, it's the main animation loop
 function draw() {
+  //background(220);    
+  // call a method on the instance
+  myInstance.myMethod();
+
   noStroke();
   fill(21, 0, 63, 20);
   rect(0, 0, width, height);
@@ -112,6 +125,11 @@ function draw() {
   drawTreeLayers();
 }
 
+// mousePressed() function is called once after every time a mouse button is pressed
+function mousePressed() {
+    // code to run when mouse is pressed
+}
+
 function generateTrees() {
   treeLayers = [];
 
@@ -135,7 +153,7 @@ function generateTrees() {
 function drawHillLayer() {
   noStroke();
   fill(0,25,10);
-  circle(width/2, 5.25*height/3, 1500)
+  circle(width/2, 5.3*height/3, 1500)
 }
 
 function drawTreeLayers() {
